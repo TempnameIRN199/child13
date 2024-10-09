ec61e9b2769cfd35f11e5a36408c3d0e8d67b1e8
kuma <vityayuzbekov602@gmail.com>
2024-10-09 22:49:47 +0300

    refactor: оновлено структуру та логіку додаткового вікна
    - Змінено порядок документів у групі DocumentGroupContainers.
    - Оновлено клас Coordinate для підтримки нових методів встановлення та отримання координат.

--- child13/Models/Coordinate.cs
+++ child13/Models/Coordinate.cs
@@ -8,26 +8,5 @@ namespace child13.Models
 {
     public class Coordinate
     {
-        public int x1, x2, y1, y2;
-
-        public Coordinate()
-        {
-        }
-
-        // Установка новых координат для передачи в другие классы
-        public void GetCoordinates(int x1, int y1, int x2, int y2)
-        {
-            this.x1 = x1;
-            this.y1 = y1;
-            this.x2 = x2;
-            this.y2 = y2;
-        }
-
-        // Передача координат в другие классы
-        public int[] SetCoordinates()
-        {
-            int[] coordinates = { x1, y1, x2, y2 };
-            return coordinates;
-        }
     }
 }
